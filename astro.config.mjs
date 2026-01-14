6d7a462b0103dd2cf50515a6a620f08d564ea5ee
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2026-01-14 22:51:54 +0000

    feat(*): :sparkles: global styles and initial layout for home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [mdx()]
 });
