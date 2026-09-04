dce0510d4007701a1c8985bc116e18f722f57d67
Vaibhav Shringarpure <vamdigitalltd@gmail.com>
2026-09-04 13:02:45 +0100

    chore(prettier): add prettier setup

---
+++ prettier.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  proseWrap: 'never',
+  endOfLine: 'lf',
+  singleAttributePerLine: false,
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+};
